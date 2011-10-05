6ed6cc82baf49100616c2f59d7cf338ae8aa9304
Bernerd Schaefer <bj.schaefer@gmail.com>
2011-10-05 14:12:51 +0200

    SVG::Canvas xml is valid SVG

--- lib/svg/canvas.rb
+++ lib/svg/canvas.rb
@@ -3,7 +3,17 @@ module SVG
 
     # @return [XML::Document] the raw XML document for this canvas
     def raw
-      @raw ||= XML::Document.new
+      @raw ||= XML::Document.new.tap do |document|
+        document.root = root
+      end
+    end
+
+    # @return [XML::Node] the root node for this canvas
+    def root
+      @root ||= XML::Node.new("svg").tap do |root|
+        root["version"] = "1.1"
+        root["xmlns"] = "http://www.w3.org/2000/svg"
+      end
     end
 
   end
--- spec/lib/svg/canvas_spec.rb
+++ spec/lib/svg/canvas_spec.rb
@@ -2,11 +2,27 @@ require "spec_helper"
 
 describe SVG::Canvas do
 
-  describe "#raw" do
-    let(:canvas) { SVG::Canvas.new }
+  let(:canvas) { SVG::Canvas.new }
 
+  describe "#raw" do
     it "returns the raw libxml document" do
       canvas.raw.should be_an_instance_of LibXML::XML::Document
+    end
+
+    its "root element is <svg>" do
+      canvas.raw.root.name.should eq "svg"
+    end
+
+    describe "<svg>" do
+      let(:root) { canvas.raw.root }
+
+      its "namespace is http://www.w3.org/2000/svg" do
+        root["xmlns"].should eq "http://www.w3.org/2000/svg"
+      end
+
+      its "version is 1.1" do
+        root["version"].should eq "1.1"
+      end
     end
   end
 
