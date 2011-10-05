df59547c2cb425ed0e0fea53492e5f85b170cfb9
Bernerd Schaefer <bj.schaefer@gmail.com>
2011-10-05 17:05:01 +0200

    Implement line_to, move_to, and close_path operators

--- lib/svg/node/path.rb
+++ lib/svg/node/path.rb
@@ -6,6 +6,54 @@ module SVG
         super "path"
       end
 
+      # Closes the current subpath by drawing a straight line from the current
+      # point to current subpath's initial point. Equivalent to "Z".
+      def close_path
+        append_path_data "Z"
+      end
+
+      # Draws a line from the current point to the given relative (x,y)
+      # coordinate which becomes the new current point. Equivalent to "L".
+      #
+      # @param x the x coordinate to move to
+      # @param y the y coordinate to move to
+      def line_to(x, y)
+        append_path_data "l #{x} #{y}"
+      end
+
+      # Draws a line from the current point to the given absolute (x,y)
+      # coordinate which becomes the new current point. Equivalent to "L".
+      #
+      # @param x the x coordinate to draw to
+      # @param y the y coordinate to draw to
+      def line_to!(x, y)
+        append_path_data "L #{x} #{y}"
+      end
+
+      # Starts a new sub-path at the given relative (x,y) coordinate.
+      # Equivalent to "M".
+      #
+      # @param x the x coordinate to move to
+      # @param y the y coordinate to move to
+      def move_to(x, y)
+        append_path_data "m #{x} #{y}"
+      end
+
+      # Starts a new sub-path at the given absolute (x,y) coordinate.
+      # Equivalent to "M".
+      #
+      # @param x the x coordinate to move to
+      # @param y the y coordinate to move to
+      def move_to!(x, y)
+        append_path_data "M #{x} #{y}"
+      end
+
+      private
+
+      def append_path_data(data)
+        self["d"] = [self["d"], data].join(" ").strip
+      end
+
     end
   end
 end
--- spec/lib/svg/node/path_spec.rb
+++ spec/lib/svg/node/path_spec.rb
@@ -1,11 +1,62 @@
 require "spec_helper"
 
 describe SVG::Node::Path do
+  let(:path) { SVG::Node::Path.new }
+
   it "inherits from SVG::Node" do
-    SVG::Node::Path.new.should be_a_kind_of SVG::Node
+    path.should be_a_kind_of SVG::Node
   end
 
   its "name" do
-    SVG::Node::Path.new.name.should eq "path"
+    path.name.should eq "path"
   end
+
+  describe "#close_path" do
+    it "appends the path data" do
+      path.should_receive(:append_path_data).with("Z")
+      path.close_path
+    end
+  end
+
+  describe "#line_to" do
+    it "appends the path data" do
+      path.should_receive(:append_path_data).with("l 10 10")
+      path.line_to 10, 10
+    end
+  end
+
+  describe "#line_to!" do
+    it "appends the path data" do
+      path.should_receive(:append_path_data).with("L 10 10")
+      path.line_to! 10, 10
+    end
+  end
+
+  describe "#move_to" do
+    it "appends the path data" do
+      path.should_receive(:append_path_data).with("m 10 10")
+      path.move_to 10, 10
+    end
+  end
+
+  describe "#move_to!" do
+    it "appends the path data" do
+      path.should_receive(:append_path_data).with("M 10 10")
+      path.move_to! 10, 10
+    end
+  end
+
+  context "after a sequence of commands" do
+    before do
+      path.move_to 10, 10
+      path.line_to 10, 100
+      path.line_to 100, 100
+      path.close_path
+    end
+
+    its "d" do
+      path["d"].should eq "m 10 10 l 10 100 l 100 100 Z"
+    end
+  end
+
 end
